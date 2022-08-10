ae3108a916d0ce3d8266e3aabfd0ad19ef216f1c
unknown <cabaraul@gmail.com>
2022-08-10 19:14:04 -0400

    adding places controller

--- rest-rant/index.js
+++ rest-rant/index.js
@@ -7,39 +7,46 @@ app.get('/', function(req, res){
         res.send("HomePage")
 })
 
-app.get('/places/', function(req, res){
+
+
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.use('/places', require('./controllers/places'))
+// app.get('/places/', function(req, res){
 
     
-            res.send("List of Places")
+//             res.send("List of Places")
 
-})
+// })
 
-app.get('/places/new', function(req, res){
+// app.get('/places/new', function(req, res){
 
     
-            res.send('Form')
+//             res.send('Form')
 
-})
+// })
 
-app.get('/places/:id', function(req, res){
+// app.get('/places/:id', function(req, res){
 
-    res.send( req.params.id
-    )
-})
-
-
-app.get('/places/:id/edit', function(req, res){
-
-    res.send( `Edit ${req.params.id}`
-    )
-})
+//     res.send( req.params.id
+//     )
+// })
 
 
-app.post('/places/', function(req, res){
+// app.get('/places/:id/edit', function(req, res){
 
-    res.send( `Create new place`
-    )
-})
+//     res.send( `Edit ${req.params.id}`
+//     )
+// })
+
+
+// app.post('/places/', function(req, res){
+
+//     res.send( `Create new place`
+//     )
+// })
 
 app.listen(process.env.PORT, function(){
     console.log("server runing")
