360fe4cdc70d7ca92f00f80d7b4ed5c1f2bdada5
unknown <cabaraul@gmail.com>
2022-08-12 10:15:02 -0400

    Adding 404 page

--- rest-rant/index.js
+++ rest-rant/index.js
@@ -1,56 +1,18 @@
-require('dotenv').config();
-const express= require('express')
-const app= express();
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
-app.get('/', function(req, res){
 
-        res.send("HomePage")
+app.get('/', (req, res) => {
+  res.render('home')
 })
-
-
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+  res.render('error404')
 })
 
-
-
-// app.get('/places/', function(req, res){
-
-    
-//             res.send("List of Places")
-
-// })
-
-// app.get('/places/new', function(req, res){
-
-    
-//             res.send('Form')
-
-// })
-
-// app.get('/places/:id', function(req, res){
-
-//     res.send( req.params.id
-//     )
-// })
-
-
-// app.get('/places/:id/edit', function(req, res){
-
-//     res.send( `Edit ${req.params.id}`
-//     )
-// })
-
-
-// app.post('/places/', function(req, res){
-
-//     res.send( `Create new place`
-//     )
-// })
-
-app.listen(process.env.PORT, function(){
-    console.log("server runing")
-})
-
+app.listen(process.env.PORT)
