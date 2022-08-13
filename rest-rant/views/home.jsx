0842feb2b04e53756760b7e5d49e807641ca5c7a
unknown <cabaraul@gmail.com>
2022-08-13 11:50:34 -0400

    adding images

--- rest-rant/views/home.jsx
+++ rest-rant/views/home.jsx
@@ -8,8 +8,12 @@ function home () {
     <Def>
         <main>
             <h1>HOME</h1>
+            <div className='home'>
+              <img src="/images/chrisrest.jpg" alt="restaurant"></img>
+            </div>
         </main>
-        <a href="/places">
+        <a href="/places" className='button'>
+    
   <button className="btn btn-primary">Places Page</button>
 </a>
 
--- rest-rant/views/places/index.jsx
+++ rest-rant/views/places/index.jsx
@@ -6,20 +6,24 @@ const Def = require('../default')
 function index (data) {
   let placesFormatted = data.places.map((place) => {
     return (
-      <div>
-        <h2>{place.name}</h2>
-        <img src={place.pic} alt={place.name}/>
+      <div className="col-sm-6">
+        <h2 className='text-center'>{place.name}</h2>
+        <p className='text-center'>{place.cuisines}</p>
+        <img className='center' src={place.pic} alt={place.name} />
       </div>
     )
   })
   return (
     <Def>
         <main>
-            <h1>PLACES INDEX PAGE</h1>
-            {placesFormatted}
+            <h1>Places to Rant or Rave About</h1>
+            <div className="row">
+              {placesFormatted}
+            </div>
         </main>
     </Def>
-)
+  )
 }
+
 
 module.exports = index
