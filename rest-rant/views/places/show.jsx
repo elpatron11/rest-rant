a698975c0646663f45c770e77e4375687f75dff6
unknown <cabaraul@gmail.com>
2022-08-15 19:20:05 -0400

    adding delete and edit buttons

---
+++ rest-rant/views/places/show.jsx
@@ -0,0 +1,33 @@
+const React = require('react')
+const Def = require('../default')
+
+function show (data) {
+    return (
+        <Def>
+          <main>
+            <h1>{ data.place.name }</h1>
+            
+		    <p className="text-center">
+		      {data.place.cuisines}
+		    </p>
+		    <img src={data.place.pic} alt={data.place.name} height="40%" width="100%"/>
+		    <p className="text-center">
+		      Located in {data.place.city}, {data.place.state}
+		    </p>
+     <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+  Edit
+</a>     
+ 
+<form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+  <button type="submit" className="btn btn-danger">
+    Delete
+  </button>
+</form> 
+ 
+
+          </main>
+        </Def>
+    )
+}
+
+module.exports = show
